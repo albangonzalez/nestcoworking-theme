c6a7e40b31874a379e17db66bb4d881765bad63c
Alban González <alban.gonzalez@outlook.com>
2026-06-23 15:59:47 -0500

    Latest posts pattern

---
+++ patterns/latest-posts.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Latest Posts
+ * Slug: nestcoworking/latest-posts
+ * Categories: posts
+ * Block Types: core/query
+ */
+?>
+<!-- wp:query {"align":"full","query":{"perPage":3,"inherit":false}} -->
+<div class="wp-block-query alignfull">
+<!-- wp:post-template {"className":"bento-grid"} -->
+    <!-- wp:post-featured-image {"isLink":true,"className":"rounded"} /-->
+    <!-- wp:post-title /-->
+<!-- /wp:post-template -->
+</div>
+<!-- /wp:query -->
